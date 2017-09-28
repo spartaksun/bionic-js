505a1a15ae7ccc7d1cddd1ca223cc86cfca33e3b
Alexey Belyakovskiy <alexeyb@compandsave.com>
2017-09-28 14:01:42 +0300

    Fix render question list

--- src/components/pages/questions/list.page.js
+++ src/components/pages/questions/list.page.js
@@ -12,13 +12,16 @@ class QuestionListPage extends Page {
     constructor(url) {
         super(url);
 
+        this.title = 'Question list';
         this.content = pageContent;
+    }
+
+    beforeRender() {
         const questions = db.findAll('questions', ['~createdAt']).map(question => ({
             ...question,
             created: moment.unix(question.createdAt).format('Y-m-d HH:mm')
         }));
 
-        this.title = 'Question list';
         this.data = {
             questions
         }
